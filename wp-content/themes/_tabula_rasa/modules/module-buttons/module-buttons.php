3d0010b2ac3210a6f8258828eb35cf09a98ab756
Kyle Marcy <k.marcy@northeastern.edu>
2020-08-09 09:29:18 -0400

    added in the buttons module (basics)

--- wp-content/themes/_tabula_rasa/modules/module-buttons/module-buttons.php
+++ wp-content/themes/_tabula_rasa/modules/module-buttons/module-buttons.php
@@ -1,17 +1,58 @@
-<?php 
+<?php
 /**
  * 
  */
 
 	$return = '';
-	$guides = [''];
+
+	
+  $guides = [
+    '<section class="tr__module tr__module-'.$mC['acf_fc_layout'].'">
+      <div class="container %1$s">
+        %2$s
+      </div>
+		</section>'
+	];
+	
+
+
+	$btnReturn = '';
+	$btnGuides = [
+		'<a href="%1$s" title="%2$s%5$s" class="button %3$s"%4$s><span>%2$s</span></a>'
+	];
+	
+	if( !empty($mC['buttons']) ){
+		
+		$btnReturn = '<div class="buttons align-'.$mC['options']['alignment'].'">';
+		
+
+		// 
+		foreach( $mC['buttons'] as $rec ){
+
+
+			$btnReturn .= sprintf(
+				$btnGuides[0]
+				,$rec['link']['url']
+				,$rec['text']
+				,$rec['style']
+				,!empty($rec['link']['target']) ? ' target="_blank"' : ''
+				,!empty($rec['link']['target']) ? ' [will open in new tab/window]' : ''
+			);
+			
+		}
+		$btnReturn .= '</div>';
+	}
+
 
 	
 	$return .= sprintf(
 		$guides[0]
+		,$mC['width']
+		,$btnReturn
 	);
-	
-  
-  echo $return;
 
- ?>
+
+	echo $return;
+
+	
+?>
